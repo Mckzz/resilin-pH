edae51e3257f5d3bd8253a3ff6ba93e4078608bb
unknown <emckenzie@zoology.ubc.ca>
2020-11-07 23:13:08 -0800

    trimmed

--- sac pH.R
+++ sac pH.R
@@ -12,9 +12,6 @@ is.data.frame(pH_series)
 print(pH_series)
 str(pH_series)
 
-
-
-
 pH_series <- as_tibble(pH_series)
 
 pH_series$pH <- as.factor(pH_series$pH)
@@ -37,29 +34,29 @@ stripchart(diff~pH, data= pH_series, vertical = TRUE, method = "jitter",
 
 ###
 
-#stripchart(diff ~ pH, data = pH_series, vertical = TRUE, method = "jitter", pch = 1)
-#m <- tapply(pH_series$diff, pH_series$pH, mean, na.rm=TRUE)
-#se <- tapply(pH_series$diff, pH_series$pH, 
-#             function(y){ sd(y, na.rm=TRUE)/sqrt(length(na.omit(y))) })
-#points( m ~ c(2:11 + 0.2) + 0.2, pch=16, col = "red")
-#segments(x0 = c(2:11 + 0.2), y0 = m - se, 
-#   x1 = c(2:11 + 0.2), y1 = m + se, col = "red")
+stripchart(diff ~ pH, data = pH_series, vertical = TRUE, method = "jitter", pch = 1)
+m <- tapply(pH_series$diff, pH_series$pH, mean, na.rm=TRUE)
+se <- tapply(pH_series$diff, pH_series$pH, 
+             function(y){ sd(y, na.rm=TRUE)/sqrt(length(na.omit(y))) })
+points( m ~ c(2:11 + 0.2) + 0.2, pch=16, col = "red")
+segments(x0 = c(2:11 + 0.2), y0 = m - se, 
+   x1 = c(2:11 + 0.2), y1 = m + se, col = "red")
 
 
 
-#e <- ggplot(pH_series, aes(x = pH, y = diff))
-#e + stat_summary(fun.data="mean_sdl",  fun.args = list(mult=1))
+e <- ggplot(pH_series, aes(x = pH, y = diff))
+e + stat_summary(fun.data="mean_sdl",  fun.args = list(mult=1))
 
-#plot(pH_series$diff~pH_series$pH, data= pH_series)
+plot(pH_series$diff~pH_series$pH, data= pH_series)
 
 
-#ggplot(pH_series, aes(x=pH, y=diff)) + 
-# geom_jitter(size = 2, width = 0.1) +
-#stat_summary(fun.data = mean_sdl, 
-#            geom = "errorbar", 
-#           fun.args = list(mult=1),
-#          width = 0.1, 
-#         position=position_nudge(x = 0))
+ggplot(pH_series, aes(x=pH, y=diff)) + 
+ geom_jitter(size = 2, width = 0.1) +
+stat_summary(fun.data = mean_sdl, 
+            geom = "errorbar", 
+           fun.args = list(mult=1),
+          width = 0.1, 
+         position=position_nudge(x = 0))
 
 
 
